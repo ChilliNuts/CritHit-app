553e76aa39416e0c4738329e66926dc4f110d34e
ChilliNuts <johnylawless@gmail.com>
2021-05-23 16:55:45 +1000

    Added crit info modal
    Generator now adds to an array, which is then displayed inside a popup modal.

--- generator.js
+++ generator.js
@@ -5,21 +5,60 @@ const rule = document.querySelector("#rule");
 const healing = document.querySelector("#healing");
 const severity = document.querySelector("#severity");
 
+const critInfo = document.querySelector("#critInfo");
+
 
 const btns = document.querySelectorAll(".weapon");
+
+const outcomes = [];
 
 btns.forEach(btn => {
 
    btn.addEventListener('click', event => {
     const outcome = finalOutcome(event.target.id);
-    severity.innerHTML = "SEVERITY: " + outcome.sev;
-    weap.innerHTML = event.target.id;
-    desc.innerHTML = outcome.flavour;
-    rule.innerHTML = outcome.rule;
-    healing.innerHTML = outcome.healing;
+    outcome.weap = event.target.id;
+
+    outcomes.unshift(outcome);
+
+    showOutcomes();
+
+    // severity.innerHTML = "SEVERITY: " + outcome.sev;
+    // weap.innerHTML = event.target.id;
+    // desc.innerHTML = outcome.flavour;
+    // rule.innerHTML = outcome.rule;
+    // healing.innerHTML = outcome.healing;
    });
 
 });
+
+function showOutcomes(){
+    let firstHit = true;
+    
+    critInfo.innerHTML = "";
+
+    outcomes.forEach(info => {
+        var div = document.createElement('div');
+
+        if (!firstHit){
+            div.innerHTML = `<hr>
+            <p class="">${info.weap + "  -  SEVERITY: " + info.sev}</p>
+            `;
+        }else {
+            firstHit = false;
+            document.getElementById("infoModalTitle").innerHTML = `<p class="">${info.weap + "  -  SEVERITY: " + info.sev}</p>`
+        } 
+
+        div.innerHTML += `
+            
+            <p class="">${info.flavour}</p>
+            <p class="">Rules: ${info.rule}</p>
+            <p class="">Healing: ${info.healing}</p>
+        `;
+        critInfo.appendChild(div);
+    });
+}
+
+
 
 function calcSeverity(severity) {
     var randomNum = Math.floor(Math.random() * 100) + 1;
